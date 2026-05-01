de14fcf5f36a377922acc1def831d04b5dc88355
“beata-dan” <beata.danilevskaya@gmail.com>
2026-05-01 12:54:58 +0100

    Added getUserChoice and checkGuess

--- game.js
+++ game.js
@@ -1,3 +1,22 @@
-const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 100)
+
+//Generate a random nunmber 
+
+const getComputerChoice = (target) => {
+    const randomNumber = Math.floor(Math.random() * 100)+1
+}
+
+const getUserChoice = (input) => {
+    if(typeof input !== "number"){
+        return "Invalid input, please type a whole number"
+    }
+}
+
+function checkGuess(input, target){
+    if (input === target){
+        return "Correct!"
+    } else if (input < target){
+        return "Too low!"
+    } else {
+        return "Too high!"
+    }
 }
